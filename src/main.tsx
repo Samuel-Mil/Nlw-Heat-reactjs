f32a4366c6e76a9d34fcd77136ea4c3520a14c2f
Samuel Milhomens <samuel.mil.mar@gmail.com>
2021-10-20 21:47:19 -0300

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "./styles/global.css"
+import { AuthProvider } from "./contexts/auth";
+import { App } from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
